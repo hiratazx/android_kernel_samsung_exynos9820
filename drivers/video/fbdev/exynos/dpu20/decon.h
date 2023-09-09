645e142c4f6af291b3304a7455a3152348f50aef
Tim Zimmermann <tim@linux4.de>
2023-09-09 16:04:32 +0200

    video: fbdev: exynos: dpu20: Support WIN_STATE_MRESOL based multi-resolution change
    * Missing pieces taken from Exynos2100
    
    Change-Id: I707dc457c581a741f01e7c4b0d8e86cff648443d

--- drivers/video/fbdev/exynos/dpu20/decon.h
+++ drivers/video/fbdev/exynos/dpu20/decon.h
@@ -549,12 +549,14 @@ struct decon_win_config_extra {
 struct decon_win_config_data_old {
 	int	retire_fence;
 	int	fd_odma;
+	u32	fps;
 	struct decon_win_config config[MAX_DECON_WIN + 1];
 };
 
 struct decon_win_config_data {
 	int	retire_fence;
 	int	fd_odma;
+	u32	fps;
 	struct decon_win_config config[MAX_DECON_WIN + 1];
 	struct decon_win_config_extra extra;
 };
--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -2629,6 +2629,20 @@ video_emul_check_done:
 			goto end;
 		}
 	} else {
+		if (regs->dpp_config[DECON_WIN_UPDATE_IDX].state ==
+				DECON_WIN_STATE_MRESOL) {
+			if (regs->mres_update) {
+				if ((decon->dt.psr_mode == DECON_MIPI_COMMAND_MODE) &&
+					(decon->dt.trig_mode == DECON_HW_TRIG)) {
+					if (decon_reg_wait_update_done_timeout(decon->id,
+								SHADOW_UPDATE_TIMEOUT) < 0)
+						decon_err("%s(%d) shadow update timeout\n", __func__, __LINE__);
+					decon_reg_set_trigger(decon->id, &psr, DECON_TRIG_DISABLE);
+				}
+				dpu_set_mres_config(decon, regs);
+			}
+		}
+
 		decon_save_cur_buf_info(decon, regs);
 		decon_wait_for_vsync(decon, VSYNC_TIMEOUT_MSEC);
 		goto end;

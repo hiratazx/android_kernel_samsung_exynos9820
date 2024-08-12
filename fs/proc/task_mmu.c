63b0956933219f7c149f8af60ec0f40ed2e8936a
Raka Prasetya Pratama <madeayurenapramudya@gmail.com>
2024-08-12 03:38:22 +0000

    susfs: Introducing susfs https://gitlab.com/simonpunk/susfs4ksu
    Signed-off-by: Raka Prasetya Pratama <madeayurenapramudya@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -28,6 +28,10 @@
 #include <asm/tlbflush.h>
 #include "internal.h"
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
 #ifdef CONFIG_ZRAM_LRU_WRITEBACK
 #include <linux/delay.h>
 #include "../../drivers/block/zram/zram_drv.h"
@@ -390,6 +394,12 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	dev_t dev = 0;
 	const char *name = NULL;
 
+#ifdef CONFIG_KSU_SUSFS_SUS_MAPS
+       char *out_name;
+       int ret = 0;
+#endif
+
+
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 		dev = inode->i_sb->s_dev;
@@ -399,6 +409,27 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 
 	start = vma->vm_start;
 	end = vma->vm_end;
+
+#ifdef CONFIG_KSU_SUSFS_SUS_MAPS
+       out_name = kmalloc(SUSFS_MAX_LEN_PATHNAME, GFP_KERNEL);
+       if (!out_name)
+	       goto orig_flow;
+       ret = susfs_sus_maps(ino, end - start, &ino, &dev, &flags, &pgoff, vma, out_name);
+
+orig_flow:
+#endif
+
+#ifdef CONFIG_KSU_SUSFS_SUS_MAPS
+       if (ret == 2) {
+	       seq_pad(m, ' ');
+	       seq_puts(m, out_name);
+	       seq_putc(m, '\n');
+	       kfree(out_name);
+	       return;
+       }
+       kfree(out_name);
+#endif
+
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
 
 	/*
